b4feb6e57a1cff0acb7f7e55db079d58bb4a0caf
naufalzian <166086118+naufalzian@users.noreply.github.com>
2024-04-22 15:25:54 +0700

    Add files via upload

---
+++ Latihan Object 1/scriptobject1.js
@@ -0,0 +1,77 @@
+const barang =[
+{
+    kode_barang: "KPLPI-023",
+    Nama_produk: "Kapal Api",
+    harga: "1500",
+    stock:  20,
+},
+{
+    kode_barang: "NBTI-112",
+    Nama_produk: "Nabati Keju",
+    harga: "1000",
+    stock:  12,
+},
+{
+    kode_barang: "SHPH&SLDR-004=",
+    Nama_produk: "Shampo Head & Shoulders",
+    harga: "7000",
+    stock:  36,
+},
+{
+    kode_barang: "TOPKPI-230",
+    Nama_produk: "Top Kopi Gula Aren",
+    harga: "5000",
+    stock:  60,
+},
+
+];
+console.log(
+  "====================================================================="
+);
+console.log("Kode Barang\t\t\t\tNama Produk\t\t\t\tHarga\t\t\tStock");
+console.log(
+  "====================================================================="
+);
+console.log(
+  barang[0].kode_barang +
+    "\t\t\t" +
+    barang[0].nama_produk +
+    "\t\t\t" +
+    barang[0].harga +
+    "\t\t\t" +
+    barang[0].stock +
+    "\t\t\t"
+);
+console.log(
+  barang[1].kode_barang +
+    "\t\t\t" +
+    barang[1].nama_produk +
+    "\t\t" +
+    barang[1].harga +
+    "\t\t\t" +
+    barang[1].stock +
+    "\t\t\t"
+);
+console.log(
+  barang[2].kode_barang +
+    "\t\t\t" +
+    barang[2].nama_produk +
+    "\t\t" +
+    barang[2].harga +
+    "\t\t\t" +
+    barang[2].stock +
+    "\t\t\t"
+);
+console.log(
+  barang[3].kode_barang +
+    "\t\t\t" +
+    barang[3].nama_produk +
+    "\t\t\t" +
+    barang[3].harga +
+    "\t\t\t" +
+    barang[3].stock +
+    "\t\t\t"
+);
+console.log(
+  "====================================================================="
+);
